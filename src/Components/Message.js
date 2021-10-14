17da98389a5a5e6d4de7cc72a766c17bbeb9a15f
Bruno Rodrigo <brunnogost2017@gmail.com>
2021-10-14 20:26:04 -0300

    atividade components

---
+++ src/Components/Message.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function message() {
+    return(
+        <section>
+            <h2>Mamãe sempre dizia, "Não perca! Quanto mais escura a noite... Mais brilhante as estrelas"</h2>
+            <h3>-Braum - League of Legends</h3>
+        </section>
+    )
+}
